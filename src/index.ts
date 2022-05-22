7f066c3ebe4e691b4ea302bcfcb90bc898c248e6
José Buschiazzo <104038315+josebuschiazzo2@users.noreply.github.com>
2022-05-21 21:01:37 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,33 @@
-/*Ejercicio – Par/Impar B
-•Realizar un algoritmo que dado un número entero ingresado por el usuario, visualice en pantalla si es par o impar
-En el caso de ingresar un cero, se debe volver a pedir el número por teclado (hasta que se ingrese un número mayor que cero)*/
+/*Potencias:
+Realice un programa que devuelva la potencia de un número. 
+La base y el exponente deben ser ingresados por teclado. 
+Sólo deben admitirse exponentes mayores o iguales a cero. 
+Recuerde que el resultado de un numero elevado a 0 es 1.
+Separe la lógica de calcular la potencia utilizando métodos. */
 
-let greater_zero:boolean=true;
+let greater_zero: boolean = true; // Lo uso para evaluar si es mayor a cero
+let total :number=1;
+
 
 console.clear();
-while (greater_zero){
-  let num : number = Number (prompt("Ingrese el numero a evaluar"));
-  // pregunto primero si introdujo números, entero y si es mayor a 0, sino vuelvo a pedirlo
-  if (isNaN(num) || (num%1) || (num===0)){
-    alert("Solos se adminte un número entero y Mayor a Cero (0)");
-    } else {
-        greater_zero=false;
-        if (num%2===0) { 
-          console.log ("Su número es Par");
-          } else {
-         console.log ("Su número es impar");
-              }
-        }
-  }
+
+//Bucle para obligarlo a que me ponga un número mayor o igual a 0
+while (greater_zero)
+{
+ let base: number = Number(prompt("Ingrese la Base (solo números enteros mayor igual a 0) "));
+ // pregunto primero si introdujo números, entero y si es mayor a 0, sino vuelvo a pedirlo
+ let exponent: number = Number(prompt("Ingrese el Exponente (solo números enteros mayor igual a 0) "));
+  if (isNaN(base) || isNaN(exponent) || base % 1 || exponent % 1 || base < 0 || exponent < 0) 
+  {
+      alert("Solos se adminten un números enteros y Mayor-Igual a Cero (0)");
+  } else {
+        greater_zero = false;
+        } // Salgo del bucle y hago la operación
+}
+
+for (let i:number=0;i<exponent;i++) // si el exponente es igual a 0, la variable total queda en "1" porque no entra
+{
+  total*=base;
+}
+
+console.log ("El resultado de " + base + " elevado a la " + exponent + " es igual a " + total);
